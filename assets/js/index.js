4b8cadd27ef535de1555a8336e893f6f5bbdf69a
Emerson Espinoza <51926375+emersonxinay@users.noreply.github.com>
2023-01-28 15:57:42 -0500

    fix date time

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // VARIABLES
   //===
 
-  const DATE_TARGET = new Date("12/31/2022 0:01 AM");
+  const DATE_TARGET = new Date("12/31/2024 0:01 AM");
 
   // DOM for render
   const SPAN_DAYS = document.querySelector("span#days");
